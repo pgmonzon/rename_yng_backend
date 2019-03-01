3c7ea2d720957b34d59a4a51442e4e8775009b4a
Patricio <pgmonzon@gmail.com>
2019-03-01 12:31:43 -0300

    Emprolijado inicial

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': '1962laser',
+  'database': 'mongodb://yngRldd:laser1962@198.100.45.12:27017/yangeeReloaded'
+};
--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,37 @@
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
 const app = express();
 
-const { mongoose } = require('./database');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const config = require('./config')
 
 // Settings
+// ********
+// Port
 app.set('port', process.env.PORT || 3113)
+// db
+mongoose.connect(config.database, { useNewUrlParser: true })
+  .then(db => console.log('MongoDB'))
+  .catch(err => console.error(err));
+mongoose.Promise = global.Promise;
+
+app.set('superSecret', config.secret);
 
 // Middlewares
+// ***********
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors({origin: 'http://localhost:4200'}));
 
 // Routes
+// ******
 app.use('/api/roles', require('./routes/roles.routes'));
 
-//Starting the server
+// Starting the server
+// *******************
 app.listen(app.get('port'), () => {
   console.log('Escuchando en el ', app.get('port'));
 });
